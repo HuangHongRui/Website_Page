68577f649b24e2d8bea8a14dbe0c1152d6649931
Rui <464362353@qq.com>
2017-08-02 22:50:28 +0800

    配置Ｗｅｂｐａｃｋ :octocat:

---
+++ src/js/app/index.js
@@ -0,0 +1,14 @@
+import a  from 'mod/a.js';
+
+require('css/index.less');
+require('css/b.scss');
+
+
+let ccc = 'hahaha'
+
+let aaa = 'wahaha'
+
+const $ = require('jquery')
+alert($.ajax);
+console.log($)
+
